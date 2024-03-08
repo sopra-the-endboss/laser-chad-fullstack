1f5665261a6cfb109f8e7b07d518f07ae725f524
nicolaspetermeyer <nicolas.meyer1@outlook.com>
2024-03-08 13:35:37 +0100

    Add new UI components and css

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,12 +1,47 @@
+import { Box, Typography } from "@mui/material";
+import sport from "../assets/sports.jpg";
+import art from "../assets/art.jpg";
+import edu from "../assets/edu.jpg";
 import React from "react";
+import ProductCategories from "./ProductCategories";
 
-const Home = () => {
+function Home() {
   return (
     <div>
-      <h1>Welcome to our E-Commerce Webstore!</h1>
-      {/* Add your content here */}
+      <Box>
+        <Typography variant="h3" component="p">
+          HPLaserChads E-Commerce Solution
+        </Typography>
+
+        <Box className="roundedD"></Box>
+      </Box>
+      <Box className="p-8">
+        <Typography variant="h5" component="p" sx={{ color: "grey" }}>
+          Welcome to the LaserChads
+        </Typography>
+        <Box py={2} sx={{ textAlign: "center", width: "70%", margin: "auto" }}>
+          <Typography variant="p" component="p" sx={{ color: "grey" }}>
+            HPLaserChads E-Commerce Solution is a platform where you can buy and
+            sell products. We have a wide range of products from different
+            merchants. You can buy and sell products of your choice.
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "1100px",
+          margin: "auto",
+          flexWrap: "wrap",
+        }}
+      >
+        <ProductCategories img={sport} title={"Sports"} />
+        <ProductCategories img={art} title={"Arts"} />
+        <ProductCategories img={edu} title={"Edcation"} />
+      </Box>
     </div>
   );
-};
+}
 
 export default Home;
