a3ae363bd62c3102ceea934cdd4558d432d0c536
dani <lutzidan@students.zhaw.ch>
2024-03-11 13:21:12 +0100

    modified navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,19 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* Add your routes here */}
-        </Routes>
+        <Navigation />
+          <div style={{marginTop: "80px"}}>
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  {/* Add your routes here */}
+              </Routes>
+          </div>
       </BrowserRouter>
     </div>
   );
