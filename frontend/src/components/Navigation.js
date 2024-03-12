695f22116c2d61ed7e90876e8b3ce8cdc6f2b1f5
daniellutziger <daniel.lutziger@uzh.ch>
2024-03-12 12:49:14 +0100

    refactoring removing warnings

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import AllProductsnameMock from "../data/AllProductsNameMock.json"
 import {useNavigate} from "react-router-dom";
