4b36e54d45f8a4abff2971aa6ced26f25bf42156
daniellutziger <daniel.lutziger@uzh.ch>
2024-03-12 11:37:00 +0100

    basic basic product browsing implemented.

--- frontend/src/views/ProductDetail.js
+++ frontend/src/views/ProductDetail.js
@@ -1,4 +1,4 @@
-import {Container, Grid} from "@mui/material";
+import {Container, Grid, Paper, Rating} from "@mui/material";
 import React, {useEffect, useState} from "react";
 import ProductDetails from "../data/ProductDetails.json"
 import ProductComments from "../data/ProductComments.json"
@@ -7,10 +7,12 @@ import CarouselComponent from "../components/ProductOverview/CarouselComponent";
 import ProductComponent from "../components/ProductOverview/ProductComponent";
 import sport from "../assets/sports.jpg";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 export const ProductDetail = () => {
 
-    const [productDetails, setProductDetails] = useState({});
+    const [productDetails, setProductDetails] = useState({technical_details: {}});
+    const [productComments, setProductComments] = useState([]);
     const {product_id} = useParams();
 
     useEffect(() => {
@@ -22,7 +24,11 @@ export const ProductDetail = () => {
     useEffect(() => {
         //TODO: fetch Product deatails for this stuff from backend
         //ProductComments
+        setProductComments(ProductComments.filter(productComment => productComment.product_id == product_id)[0]);
     }, [ProductComments]);
+
+    console.log(productDetails);
+    console.log(productComments);
 
     return (
         <>
@@ -32,27 +38,55 @@ export const ProductDetail = () => {
             <Grid item xs={4}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        <Typography gutterBottom variant="h3" component="div" color={"red"} align={"left"}>
-                            {productDetails.price}
+                        <Typography gutterBottom variant="h3" component="div" color="red" align="left">
+                            ${productDetails.price}
                         </Typography>
-                        <Typography gutterBottom variant="h5" component="div" color={"black"} style={{fontWeight: "bold"}} align={"left"}>
-                            {productDetails.brand} <Typography gutterBottom variant="h5" component="span" color={"black"} align={"left"}>{productDetails.product}</Typography>
+                        <Typography gutterBottom variant="h5" component="div" color="black" style={{ fontWeight: 'bold' }} align="left">
+                            {productDetails.brand} <Typography gutterBottom variant="h5" component="span" color="black" align="left">{productDetails.product}</Typography>
                         </Typography>
-                        <Typography gutterBottom variant="body2" color="text.secondary" align={"left"}>
+                        <Typography gutterBottom variant="body2" color="text.secondary" align="left">
                             {productDetails?.subheader}
                         </Typography>
-
                     </Grid>
 
                     <Grid item xs={12}>
+                        <Rating
+                            name="user-rating"
+                            value={productComments?.reviews?.reduce((acc, review) => acc + review.rating, 0) / productComments?.reviews?.length}
+                            readOnly
+                        />
+                    </Grid>
 
+                    <Grid item xs={12}>
+                        <Button variant="contained" color="primary">Buy Now</Button>
+                    </Grid>
 
+                    <Grid item xs={12}>
+                        <Button variant="outlined" color="primary">Pin Product</Button>
                     </Grid>
                 </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="h6" align="left">Specs</Typography>
+                    <Paper elevation={0} variant="outlined" square sx={{ p: 2 }}>
+                        {/* Display each technical detail */}
+                        {Object.entries(productDetails?.technical_details).map(([key, value], i) => (
+                            <Typography key={i} gutterBottom variant="body2" color="text.secondary" align="left">
+                                {`${key}: ${value}`}
+                            </Typography>
+                        ))}
+                    </Paper>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Typography variant="h6" align="left">Comments</Typography>
+                    {productComments?.reviews?.map((review, i) => (
+                        <Paper key={i} elevation={1} sx={{ p: 2, mb: 2 }}>
+                            <Typography variant="subtitle2">{review?.user}</Typography>
+                            <Typography variant="body2" color="text.secondary">{review?.comment}</Typography>
+                        </Paper>
+                    ))}
+                </Grid>
             </Grid>
-            {/* GRID */}
-            {/* 2/3 image carousel view */}
-            {/* 1/3 price */}
         </>
     )
 };
