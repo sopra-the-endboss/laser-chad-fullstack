8f94b7964f764fc93cf0b42a1e525da9f541fb36
daniellutziger <daniel.lutziger@uzh.ch>
2024-03-12 10:57:12 +0100

    made carousel adaptable for product details as well.

--- frontend/src/views/ProductDetail.js
+++ frontend/src/views/ProductDetail.js
@@ -1,8 +1,11 @@
-import {Container} from "@mui/material";
+import {Container, Grid} from "@mui/material";
 import React, {useEffect, useState} from "react";
 import ProductDetails from "../data/ProductDetails.json"
 import ProductComments from "../data/ProductComments.json"
 import {useParams} from "react-router-dom";
+import CarouselComponent from "../components/ProductOverview/CarouselComponent";
+import ProductComponent from "../components/ProductOverview/ProductComponent";
+import sport from "../assets/sports.jpg";
 
 export const ProductDetail = () => {
 
@@ -12,7 +15,7 @@ export const ProductDetail = () => {
     useEffect(() => {
         //TODO: fetch Product deatails for this stuff from backend
         // I expect an object, that's why the [0]
-        setProductDetails(ProductDetails.filter(productDetail => productDetail.id == product_id)[0]);
+        setProductDetails(ProductDetails.filter(productDetail => productDetail.product_id == product_id)[0]);
     }, [ProductDetails]);
 
     useEffect(() => {
@@ -22,6 +25,20 @@ export const ProductDetail = () => {
 
     return (
         <>
+            <Grid item xs={8}>
+                <CarouselComponent carouselData={productDetails} clickable={false}/>
+            </Grid>
+            <Grid item xs={4}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        Price
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        Rating
+                    </Grid>
+                </Grid>
+            </Grid>
             {/* GRID */}
             {/* 2/3 image carousel view */}
             {/* 1/3 price */}
