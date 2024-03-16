3a1d5aa8419c327c4775d93bdab9a37170257ce0
daniellutziger <daniel.lutziger@uzh.ch>
2024-03-16 17:11:44 +0100

    fixed dependencies and cleaned up

--- frontend/src/components/ProductOverview/CarouselComponent.js
+++ frontend/src/components/ProductOverview/CarouselComponent.js
@@ -1,39 +1,15 @@
 import * as React from 'react';
-import {useTheme} from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import {autoPlay} from 'react-swipeable-views-utils';
 import {CardActionArea} from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import Card from "@mui/material/Card";
 import {CardContentComponent} from "./CardContentComponent";
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+import Carousel from 'react-multi-carousel';
+import "react-multi-carousel/lib/styles.css";
+import CardMedia from "@mui/material/CardMedia";
 
 function CarouselComponent({carouselData, clickable = true, onCardInteract}) {
-    const theme = useTheme();
-    const [activeStep, setActiveStep] = React.useState(0);
 
     if (carouselData) {
-        const maxSteps = Array.isArray(carouselData) ? carouselData?.length : carouselData.images?.length;
-
-        const handleNext = (e) => {
-            e.stopPropagation();
-            setActiveStep((prevActiveStep) => (prevActiveStep+1) % maxSteps);
-        };
-
-        const handleBack = (e) => {
-            e.stopPropagation();
-            setActiveStep((prevActiveStep) => (prevActiveStep-1+ maxSteps) % maxSteps);
-        };
-
-        const handleStepChange = (step) => {
-            setActiveStep(step);
-        };
 
         // Normalize the carousel data to always have an 'images' array
         const normalizedCarouselData = Array.isArray(carouselData) ? carouselData.map(item => {
@@ -46,74 +22,96 @@ function CarouselComponent({carouselData, clickable = true, onCardInteract}) {
             images: carouselData.images || [carouselData.image]
         }];
 
+        const carouselContent = (item, image) => {
+            return (
+                <Card sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '100%', // Ensure the card takes up the full height
+                }} onClick={() => {
+                    clickable && onCardInteract(clickable, item?.product_id)
+                }}>
+                    <CardActionArea sx={{flexGrow: 1}}> {/* flexGrow allows the card to grow */}
+                        <CardMedia
+                            component="img"
+                            image={image}
+                            alt={"Product description"}
+                            sx={{height: 'auto', maxWidth: '100%'}} // Set height to auto
+                        />
+                        {clickable && <CardContent sx={{
+                            flexGrow: 1, // Allows card content to take up remaining space
+                            padding: 0,
+                            display: 'flex', // Using flex layout
+                            flexDirection: 'column', // Stack children vertically
+                            justifyContent: 'end' // Align content to the bottom
+                        }}>
+                            <CardContentComponent
+                                title={item?.product}
+                                formatted_text={item?.formatted_text}
+                                category={item?.category}
+                                price={item?.price}
+                                description={item?.description}
+                                height={157}
+                                brand={item?.brand}
+                            />
+                        </CardContent>}
+                    </CardActionArea>
+                </Card>
+            );
+        }
+
         return (
 
-            <Card sx={{height: "100%", maxHeight: 635}} onClick={() => {
-                clickable && onCardInteract(clickable, carouselData[activeStep]?.product_id)
-            }}>
-                <CardActionArea sx={{height: "100%"}}>
-                    <CardContent sx={{padding: 0}}>
-                        <Box sx={{flexGrow: 1}}>
-                            <AutoPlaySwipeableViews
-                                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                                index={activeStep}
-                                onChangeIndex={handleStepChange}
-                                enableMouseEvents
-                            >
-                                {normalizedCarouselData?.map((item, index) => (
-                                    item.images.map((image, imgIndex) => (
-                                        <div key={`${item?.product_id}_${imgIndex}`}>
-                                            {Math.abs(activeStep - index) <= maxSteps ? (
-                                                <Box
-                                                    component="img"
-                                                    sx={{
-                                                        display: 'block',
-                                                        overflow: 'hidden',
-                                                        width: '100%',
-                                                    }}
-                                                    src={image}
-                                                    alt={item?.product}
-                                                />
-                                            ) : null}
-                                        </div>
-                                    ))
-                                ))}
-                            </AutoPlaySwipeableViews>
-                            {clickable &&
-                                <CardContentComponent
-                                    title={carouselData[activeStep]?.product}
-                                    formatted_text={carouselData[activeStep]?.formatted_text}
-                                    category={carouselData[activeStep]?.category}
-                                    price={carouselData[activeStep]?.price}
-                                    description={carouselData[activeStep]?.description}
-                                    height={110}
-                                    brand={carouselData[activeStep]?.brand}
-                                />
-                            }
-
-                            <MobileStepper
-                                style={{backgroundColor: "white"}}
-                                steps={maxSteps}
-                                position="static"
-                                activeStep={activeStep}
-                                nextButton={
-                                    <Button
-                                        size="small"
-                                        onClick={handleNext}
-                                    >
-                                        <KeyboardArrowRight/>
-                                    </Button>
-                                }
-                                backButton={
-                                    <Button size="small" onClick={handleBack}>
-                                        <KeyboardArrowLeft/>
-                                    </Button>
-                                }
-                            />
-                        </Box>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
+            <Carousel
+                additionalTransfrom={0}
+                arrows
+                autoPlay={false}
+                autoPlaySpeed={3000}
+                centerMode={false}
+                className="carousel-style"
+                containerClass="carousel-container"
+                dotListClass=""
+                draggable
+                focusOnSelect={false}
+                infinite
+                minimumTouchDrag={80}
+                pauseOnHover
+                renderArrowsWhenDisabled={false}
+                renderButtonGroupOutside={false}
+                renderDotsOutside={false}
+                responsive={{
+                    desktop: {
+                        breakpoint: {
+                            max: 3000,
+                            min: 1024
+                        },
+                        items: 1
+                    },
+                    mobile: {
+                        breakpoint: {
+                            max: 464,
+                            min: 0
+                        },
+                        items: 1
+                    },
+                    tablet: {
+                        breakpoint: {
+                            max: 1024,
+                            min: 464
+                        },
+                        items: 1
+                    }
+                }}
+                sliderClass=""
+                slidesToSlide={1}
+                swipeable
+            >
+                {normalizedCarouselData?.map((item, index) => (
+                    item.images.map((image, imgIndex) => (
+                        carouselContent(item, image, imgIndex)
+                    ))
+                ))}
+            </Carousel>
         );
     }
 }
