c457271694baa7d4994fec11bef8a28ebffa8841
dani <lutzidan@students.zhaw.ch>
2024-03-27 18:30:55 -0500

    basic list added

--- frontend/src/views/MyShop.js
+++ frontend/src/views/MyShop.js
@@ -1,8 +1,10 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import MyShopMock from '../data/MyShopMock.json'
-import {Stack} from "@mui/material";
+import {Badge, Chip, Divider, List, ListItem, ListItemAvatar, ListItemText, Stack} from "@mui/material";
 import SellProduct from "../components/ProductManagement/SellProduct";
 import CustomModal from "../components/ui/CustomModal";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Typography from "@mui/material/Typography";
 const MyShop = () => {
 
     //verify that user is logged in has already been done by the routing guard
@@ -13,11 +15,65 @@ const MyShop = () => {
         setShopData(MyShopMock);
     }, []);
 
+    console.log(shopData)
+
     return (
-        <Stack spacing={2}>
+        <Stack spacing={2} sx={{width: "100%"}}>
+            {/* move this to top right */}
             <CustomModal openModalText={"Add new Product"}>
                 <SellProduct />
             </CustomModal>
+            {/* add list with all shop items */}
+            <List sx={{ width: '100%' }}>
+                {shopData?.map(product => (
+                    <>
+                        <ListItem
+                            secondaryAction={
+                                <Stack direction={"row"} sx={{alignItems: "center"}} spacing={2}>
+                                    <Chip label={product?.brand} color="success"/>
+                                    <Chip label={product?.category} color="success"/>
+                                    <DeleteIcon />
+                                    <DeleteIcon />
+                                </Stack>
+                            }
+                        >
+                            <ListItemAvatar>
+                                <img src={product?.images[0]} alt={"product image"} width={"40px"} height={"40px"}/>
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={
+                                    <>
+                                        {product.product}
+                                    </>
+                                }
+                                secondary={
+                                    <Stack direction={"row"} spacing={2}>
+                                        <Typography
+                                            sx={{ display: 'inline' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="red"
+                                        >
+                                            {"$ " + product.price}
+                                        </Typography>
+                                        <Typography
+                                            sx={{ display: 'inline' }}
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            {product.subheader}
+                                        </Typography>
+                                    </Stack>
+                                }
+                            />
+
+                        </ListItem>
+                        <Divider />
+                    </>
+
+                ))}
+            </List>
         </Stack>
     );
 }; export default MyShop;
