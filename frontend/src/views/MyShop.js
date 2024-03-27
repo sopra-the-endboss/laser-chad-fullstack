06309452ba6445a6503264e1c0daf4b91bb29c76
dani <lutzidan@students.zhaw.ch>
2024-03-27 23:02:51 +0000

    jeeezus christ what am i doing

--- frontend/src/views/MyShop.js
+++ frontend/src/views/MyShop.js
@@ -1,6 +1,8 @@
 import {useEffect, useState} from "react";
 import MyShopMock from '../data/MyShopMock.json'
 import {Stack} from "@mui/material";
+import SellProduct from "../components/ProductManagement/SellProduct";
+import CustomModal from "../components/ui/CustomModal";
 const MyShop = () => {
 
     //verify that user is logged in has already been done by the routing guard
@@ -13,7 +15,9 @@ const MyShop = () => {
 
     return (
         <Stack spacing={2}>
-
+            <CustomModal openModalText={"Add new Product"}>
+                <SellProduct />
+            </CustomModal>
         </Stack>
     );
 }; export default MyShop;
