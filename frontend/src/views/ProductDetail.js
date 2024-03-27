828486510f9bcfce69c50e34c562f55c18522077
dani <lutzidan@students.zhaw.ch>
2024-03-27 12:24:54 -0500

    fixed product detail page

--- frontend/src/views/ProductDetail.js
+++ frontend/src/views/ProductDetail.js
@@ -1,28 +1,100 @@
-import {Grid, Paper, Rating} from "@mui/material";
+import {
+    Badge,
+    Chip,
+    Grid,
+    Paper,
+    Rating,
+    TableBody,
+    TableCell,
+    TableRow,
+    Table,
+    Collapse,
+    Button,
+    Box, TableHead, TableContainer, IconButton
+} from "@mui/material";
 import React, {useEffect, useState} from "react";
 import ProductDetails from "../data/ProductDetails.json"
 import ProductComments from "../data/ProductComments.json"
 import {useParams} from "react-router-dom";
 import CarouselComponent from "../components/ProductOverview/CarouselComponent";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-export const ProductDetail = () => {
+function ProductDetailRow({ detailKey, detailValue }) {
+    const [open, setOpen] = useState(false);
+    const isPortsDetail = detailKey === 'ports';
 
-    const [productDetails, setProductDetails] = useState({technical_details: {}});
+    return (
+        <React.Fragment>
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                <TableCell>
+                    {isPortsDetail && (
+                        <IconButton
+                            aria-label="expand row"
+                            size="small"
+                            onClick={() => setOpen(!open)}
+                        >
+                            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                        </IconButton>
+                    )}
+                </TableCell>
+                <TableCell component="th" scope="row">{detailKey}</TableCell>
+                <TableCell align="right">{!isPortsDetail && detailValue.toString()}</TableCell>
+            </TableRow>
+            {isPortsDetail && (
+                <TableRow>
+                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <Box sx={{ margin: 1 }}>
+                                <Typography variant="h6" gutterBottom component="div">
+                                    Ports Details
+                                </Typography>
+                                <Table size="small" aria-label="purchases">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Port Type</TableCell>
+                                            <TableCell align="right">Count</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {Object.entries(detailValue).map(([port, count]) => (
+                                            <TableRow key={port}>
+                                                <TableCell component="th" scope="row">
+                                                    {port}
+                                                </TableCell>
+                                                <TableCell align="right">{count}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </Box>
+                        </Collapse>
+                    </TableCell>
+                </TableRow>
+            )}
+        </React.Fragment>
+    );
+}
+
+export const ProductDetail = ({details}) => {
+    const [open, setOpen] = useState(false);
+    const [productDetails, setProductDetails] = useState({...details, technical_details: {}});
     const [productComments, setProductComments] = useState([]);
     const {product_id} = useParams();
 
     useEffect(() => {
         //TODO: fetch Product deatails for this stuff from backend
         // I expect an object, that's why the [0]
-        setProductDetails(ProductDetails.filter(productDetail => productDetail.product_id === parseInt(product_id))[0]);
+        if (!details)
+            setProductDetails(ProductDetails.filter(productDetail => productDetail.product_id === parseInt(product_id))[0]);
     }, [product_id]);
 
     useEffect(() => {
         //TODO: fetch Product deatails for this stuff from backend
         //ProductComments
-        setProductComments(ProductComments.filter(productComment => productComment.product_id === parseInt(product_id))[0]);
+        if (!details)
+            setProductComments(ProductComments.filter(productComment => productComment.product_id === parseInt(product_id))[0]);
     }, [product_id]);
 
     return (
@@ -36,14 +108,21 @@ export const ProductDetail = () => {
                         <Typography gutterBottom variant="h3" component="div" color="red" align="left">
                             ${productDetails?.price}
                         </Typography>
-                        <Typography gutterBottom variant="h5" component="div" color="black" style={{ fontWeight: 'bold' }} align="left">
-                            {productDetails?.brand} <Typography gutterBottom variant="h5" component="span" color="black" align="left">{productDetails['product']}</Typography>
+                        <Typography gutterBottom variant="h5" component="div" color="black" style={{fontWeight: 'bold'}}
+                                    align="left">
+                            {productDetails?.brand} <Typography gutterBottom variant="h5" component="span" color="black"
+                                                                align="left">{productDetails['product']}</Typography>
                         </Typography>
                         <Typography gutterBottom variant="body2" color="text.secondary" align="left">
                             {productDetails?.subheader}
                         </Typography>
+                        {/* Description */}
+                        <Typography gutterBottom variant="body1" align="left">
+                            {productDetails?.description}
+                        </Typography>
+                        {/* Availability */}
+                        <Chip label={productDetails?.availability} color="success"/>
                     </Grid>
-
                     <Grid item xs={12}>
                         <Rating
                             name="user-rating"
@@ -51,31 +130,35 @@ export const ProductDetail = () => {
                             readOnly
                         />
                     </Grid>
-
                     <Grid item xs={12}>
                         <Button variant="contained" color="primary">Buy Now</Button>
                     </Grid>
-
                     <Grid item xs={12}>
                         <Button variant="outlined" color="primary">Pin Product</Button>
                     </Grid>
                 </Grid>
                 <Grid item xs={12}>
-                    <Typography variant="h6" align="left">Specs</Typography>
-                    <Paper elevation={0} variant="outlined" square sx={{ p: 2 }}>
-                        {/* Display each technical detail */}
-                        {Object.entries(productDetails?.technical_details)?.map(([key, value], i) => (
-                            <Typography key={i} gutterBottom variant="body2" color="text.secondary" align="left">
-                                {`${key}: ${value}`}
-                            </Typography>
-                        ))}
-                    </Paper>
+                    {/* Warranty as badge */}
+                    <Badge badgeContent={productDetails?.warranty} color="primary">
+                        <Typography variant="h6" align="left">Warranty</Typography>
+                    </Badge>
                 </Grid>
-
+                <Grid item xs={12}>
+                    <Typography variant="h6" align="left">Specs</Typography>
+                    <TableContainer component={Paper}>
+                        <Table aria-label="collapsible table">
+                            <TableBody>
+                                {Object.entries(productDetails.technical_details).map(([key, value], index) => (
+                                    <ProductDetailRow key={index} detailKey={key} detailValue={value} />
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
                 <Grid item xs={12}>
                     <Typography variant="h6" align="left">Comments</Typography>
                     {productComments?.reviews?.map((review, i) => (
-                        <Paper key={i} elevation={1} sx={{ p: 2, mb: 2 }}>
+                        <Paper key={i} elevation={1} sx={{p: 2, mb: 2}}>
                             <Typography variant="subtitle2">{review?.user}</Typography>
                             <Typography variant="body2" color="text.secondary">{review?.comment}</Typography>
                         </Paper>
