d5cd1df83f4fc6b9476a0a9eb61be47044545306
Darnol <32634736+Darnol@users.noreply.github.com>
2024-04-05 14:50:43 +0200

    Refactor backend structure
    Update template-microservice tests to newest standard
    Remove old django python service

---
+++ backend/product-microservice/test/test_product-microservice.py
@@ -0,0 +1,256 @@
+"""
+Running the file when docker compose up is running:
+to copy the file to a running container to run and run it right away in the docker network
+run this from a terminal in the host machine, not a container
+docker cp backend/product-microservice/test/test_product-microservice.py product-microservice-debugger:/app && docker exec product-microservice-debugger /bin/sh -c "python test_product-microservice.py"
+"""
+
+import boto3
+import os
+import simplejson as json
+import requests
+from pprint import PrettyPrinter
+pp = PrettyPrinter(indent=2)
+
+##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--
+# FOR MANUAL RUNING ONLY
+IN_DOCKER = os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False)
+
+if not IN_DOCKER:
+    print("Running script in non-docker mode")
+    os.chdir("./product-microservice")
+    # Also set all AWS env vars, point to running localstack container not in a docker-compose network
+    os.environ['AWS_DEFAULT_REGION']='us-east-1'
+    os.environ['AWS_ENDPOINT_URL']='https://localhost.localstack.cloud:4566' # For manual, use the default localstack url
+    os.environ['AWS_ACCESS_KEY_ID']='fakecred' # Sometimes boto3 needs credentials
+    os.environ['AWS_SECRET_ACCESS_KEY']='fakecred' # Sometimes boto3 needs credentials
+    os.environ['APIG_TAG'] = "apig-main"
+    os.environ['APIG_TAG_ID'] = "API_TAG_ID"
+    os.environ['APIG_STAGE'] = "PROD"
+    os.environ['PROTOCOL'] = "https"
+    os.environ['APIG_WAIT'] = "300"
+# FOR MANUAL RUNING ONLY END
+##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--
+
+import deploy_utils
+
+# Create clients
+lambda_client = boto3.client("lambda")
+apig_client = boto3.client("apigateway")
+
+#api_stage_name = os.environ['APIG_STAGE']
+api_stage_name = "PROD"
+#api_tag_id = os.environ['APIG_TAG_ID']
+api_tag_id = "API_TAG_ID"
+#api_id_to_seach = os.environ['APIG_TAG']
+api_id_to_seach = "apig-main"
+
+api_id = deploy_utils.find_api_id_by_tag(
+    apig_client,
+    tag_key = api_tag_id,
+    tag_value = api_id_to_seach
+)
+
+print("ALL APIS FOUND:\n")
+print(apig_client.get_rest_apis()['items'])
+print("-----------------")
+
+print(f"ALL DEPLOYMENTS TO {api_id} FOUND:\n")
+print(apig_client.get_deployments(restApiId=api_id)['items'])
+deployment_id = apig_client.get_deployments(restApiId=api_id)['items'][0]['id']
+print("-----------------")
+
+print(f"ALL STAGES TO {api_id} AND DEPLOYMENT {deployment_id} FOUND:\n")
+print(apig_client.get_stages(restApiId=api_id, deploymentId = deployment_id))
+print("-----------------")
+
+print(f"ALL RESOURCES TO {api_id} FOUND:\n")
+pp.pprint(apig_client.get_resources(restApiId = api_id)['items'])
+resources_id = [res['id'] for res in apig_client.get_resources(restApiId = api_id)['items']]
+print("-----------------")
+
+print(f"ALL LAMBDA FUNCTION TO {api_id} FOUND:\n")
+pp.pprint(lambda_client.list_functions())
+print("-----------------")
+
+
+###
+# Send requests to test routes /product-microservice GET und POST
+
+# if in manual mode, get corresponding protocol, in dockermode is None, defaults to http
+PROTOCOL_TO_USE = os.getenv("PROTOCOL", None)
+
+# Send request to test GET - should return empty
+url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+print(f"Sending GET to {url}")
+response = requests.get(url)
+print(response.text)
+
+# Send requests to test POST
+url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+payload =   {
+    "product_id": "1",
+    "product": "Apple iPhone 15 Pro",
+    "highlighted": True,
+    "image": "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+    "price": 57.46,
+    "formatted_text": "",
+    "category": "Smartphone",
+    "brand": "Apple"
+  }
+print(f"Sending POST to {url} with payload {json.dumps(payload)}")
+response = requests.post(url, json = payload)
+print(response.status_code)
+print(response.text)
+
+# Send requests to test GET - should return with one object
+url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+print(f"Sending GET to {url}")
+response = requests.get(url)
+print(response.text)
+
+
+# Send POST batch
+url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice/batch", protocol=PROTOCOL_TO_USE)
+payload =   [
+    {
+    "product_id": "2",
+    "product": "second product",
+    "highlighted": True,
+    "image": "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+    "price": 222,
+    "formatted_text": "",
+    "category": "two",
+    "brand": "two"},
+    {
+    "product_id": "3",
+    "product": "third product",
+    "highlighted": False,
+    "image": "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+    "price": 333,
+    "formatted_text": "",
+    "category": "three",
+    "brand": "three"
+  }
+]
+print(f"Sending POST to {url} with payload {json.dumps(payload)}")
+response = requests.post(url, json = payload)
+print(response.status_code)
+print(response.text)
+
+
+# Send requests to test GET - should return with more objects
+url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+print(f"Sending GET to {url}")
+response = requests.get(url)
+print(response.text)
+
+
+
+
+####
+# !!! OLD CODE, might have wrong request models in payload
+
+# # Send requests to test POST with invalid payload. Missing the second key -> 400
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# payload = {"product_id":1}
+# print(f"Sending POST to {url} with payload {json.dumps(payload)}")
+# response = requests.post(url, json = payload)
+# print(response.status_code)
+# print(response.text)
+
+# # Send requests to test POST with invalid payload. Not string -> 400
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# payload = {"product_id":1, "product":42}
+# print(f"Sending POST to {url} with payload {json.dumps(payload)}")
+# response = requests.post(url, json = payload)
+# print(response.status_code)
+# print(response.text)
+
+# # Send requests to test POST with invalid payload. Not number -> 400
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# payload = {"product_id":1, "product":"dummyvalue1", "product-microservice-key-numeric":"notanumber"}
+# print(f"Sending POST to {url} with payload {json.dumps(payload)}")
+# response = requests.post(url, json = payload)
+# print(response.status_code)
+# print(response.text)
+
+# # Send requests to test POST with payload with additional field, valid -> 200, we have an entry with an additional field
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# payload = {"product_id":1, "product":"dummyvalue3", "additional field":"additionalvalue"}
+# print(f"Sending POST to {url} with payload {json.dumps(payload)}")
+# response = requests.post(url, json = payload)
+# print(response.status_code)
+# print(response.text)
+
+# # Check with GET
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# print(f"Sending GET to {url}")
+# response = requests.get(url)
+# pp.pprint(response.json())
+
+# # Send POST to test the number attribute. Send valid POST
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# payload = {"product_id":2, "product":"dummyvalue3", "product-microservice-key-numeric":42}
+# print(f"Sending POST to {url} with payload {json.dumps(payload)}")
+# response = requests.post(url, json = payload)
+# print(response.status_code)
+# print(response.text)
+
+# # Check with GET
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# print(f"Sending GET to {url}")
+# response = requests.get(url)
+# pp.pprint(response.json())
+
+
+
+# ###
+# # Send requests to test /product-microservice/{id}
+# # 1 - GET without match
+# # 2 - POST item one and two
+# # 3 - GET all items
+# # 4 - GET with path without match
+# # 5 - GET wiht path with match
+
+# key_1_to_match = 3
+# item_one_to_write = {"product_id":key_1_to_match, "product":"key_1_to_match"}
+# item_two_to_write = {"product_id":4, "product":"valid_item_two"}
+# key_1_not_to_match = 5
+
+# # Send GET without match, key_1_to_match does not yet exist
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# url = url + f"/{key_1_to_match}" # Add our specific id
+# print(f"Sending GET to {url}")
+# response = requests.get(url)
+# print(response.text)
+
+# # Send POST to write two valid items
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# payload = item_one_to_write
+# print(f"Sending POST to {url} with payload {json.dumps(payload)}")
+# response = requests.post(url, json = payload)
+
+# payload = item_two_to_write
+# print(f"Sending POST to {url} with payload {json.dumps(payload)}")
+# response = requests.post(url, json = payload)
+
+# # Send request to test GET all items
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# print(f"Sending GET to {url}")
+# response = requests.get(url)
+# pp.pprint(response.json())
+
+# # Send GET without match, key_1 does not exist
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# url = url + f"/{key_1_not_to_match}" # Add our specific id
+# print(f"Sending GET to {url}")
+# response = requests.get(url)
+# pp.pprint(response.json())
+
+# # Send GET with match, id does exist
+# url = deploy_utils.get_resource_path(apig_client, api_id, stage_name = api_stage_name, resource_path = "product-microservice", protocol=PROTOCOL_TO_USE)
+# url = url + f"/{key_1_to_match}" # Add our specific id
+# print(f"Sending GET to {url}")
+# response = requests.get(url)
+# pp.pprint(response.json())
---
+++ backend/template-microservice/lambdas/post_lambda/handler.py
@@ -0,0 +1,107 @@
+"""
+Handle call which writes an item to the template DB
+TODO: Error handling (either let handler function fail, or wrap?), duplicates?
+The handler has the name of the table hardcoded, this is determined by the config file config/db_schema.json upon deployment
+"""
+import os
+import boto3
+import simplejson as json
+
+# TODO: Remove later
+from pprint import PrettyPrinter
+pp = PrettyPrinter(indent=2)
+
+HTTP_RESPONSE_DICT = {
+    'statusCode' : '', # http status code
+    'isBase64Encoded' : False, # bool
+    # To allow CORS requests from the frontend, we have to set the appropirate headers
+    # https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
+    # API Gateway Proxy Lambda integration
+    'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+            'Content-Type': 'application/json'
+        }
+    # Here comes to body, as a JSON string
+}
+
+def handler(event: dict, context) -> dict:
+    """
+    Arguments:
+        event : a dict which contains all data from the request to the API
+        context : a LambdaContext object
+    
+    Returns:
+        A valid HTTP Response dict which must contain the fields
+        - statusCode
+        - isBase64Encoded
+        - headers
+        - body
+        If headers are written, they must be a dict
+        The body must be a JSON serializable string, handeled by json.dumps()
+
+        statusCode : 200 if success, 4XX otherwise
+        isBase64Encoded : False by default
+        headers : Empty by default, dict otherwise
+        body : Empty, this function does not return anything except the statusCode
+    """
+    
+    print("post_lambda invoked")
+
+    print("DEBUG: This is the context")
+    pp.pprint(context)
+
+    print("DEBUG: This is the event")
+    pp.pprint(event)
+    
+    print("DEBUG: This is the event raw")
+    print(event)
+    
+    TableName = "template-microservice-db"
+
+    print(f"Using table {TableName}")
+
+    print("Check if table is available ...")
+    dynamo_client = boto3.client("dynamodb")
+    available_tables = dynamo_client.list_tables()
+    available_tables = available_tables['TableNames']
+    if not TableName in available_tables:
+        print(f"Table {TableName} not found in the available tables, abort")
+        HTTP_RESPONSE_DICT['statusCode'] = 400
+        HTTP_RESPONSE_DICT['body'] = json.dumps(f"Table {TableName} not found in the available tables, abort")
+        return HTTP_RESPONSE_DICT
+
+    print("Creating dynamo table object ...")
+    dynamo_resource = boto3.resource("dynamodb")
+    dynamo_table = dynamo_resource.Table(TableName)
+
+    print("Parse body")
+    try:
+        item = json.loads(event['body'])
+        print("DEBUG: This is the item")
+        print(item)
+    except json.decoder.JSONDecodeError as e:
+        print("JSONDecodeError IN PARSING BODY")
+        raise e
+
+    print("Try writing item")
+    response_put = dynamo_table.put_item(
+        TableName = TableName,
+        ReturnValues = "NONE",
+        Item = item
+    )
+
+    print("This is the response_put object from the put_item call")
+    print(response_put)
+    print("This is the response_put object from the put_item call with PrettyPrinter")
+    pp.pprint(response_put)
+
+    print("Return HTTP object")
+    HTTP_RESPONSE_DICT['statusCode'] = '200'
+    HTTP_RESPONSE_DICT['body'] = '' # We return the full dict from the dynamo.put_item method
+
+    return HTTP_RESPONSE_DICT
+    
+if __name__ == "__main__":
+    print(handler(None, None))
