cd1c5c59b4f385ee74d11172bc354315d6d59bb9
nicolaspetermeyer <nicolas.meyer1@outlook.com>
2024-04-11 15:55:28 +0200

    Add JSDoc config and documentation for various components and views

--- frontend/src/reducers/slices/authSlice.js
+++ frontend/src/reducers/slices/authSlice.js
@@ -1,36 +1,11 @@
 /**
  * Redux slice for managing the authentication state.
- *
- * @typedef {Object} AuthState - The authentication state object.
- * @property {boolean} isLoggedIn - Indicates whether the user is logged in or not.
- * @property {string|null} user - The user ID of the logged-in user.
- * @property {string|null} email - The email address of the logged-in user.
- * @property {string|null} groups - The groups the logged-in user belongs to.
- * @property {string|null} birthdate - The birthdate of the logged-in user.
- * @property {string|null} givenname - The given name of the logged-in user.
- * @property {string|null} familyname - The family name of the logged-in user.
- *
- * @typedef {Object} SetUserLoggedInAction - The action object for setting the user as logged in.
- * @property {string} userId - The user ID of the logged-in user.
- * @property {string} email - The email address of the logged-in user.
- * @property {string} groups - The groups the logged-in user belongs to.
- * @property {string} birthdate - The birthdate of the logged-in user.
- * @property {string} givenname - The given name of the logged-in user.
- * @property {string} familyname - The family name of the logged-in user.
- *
- * @typedef {Object} AuthSlice - The Redux slice object for managing the authentication state.
- * @property {string} name - The name of the slice.
- * @property {AuthState} initialState - The initial state of the slice.
- * @property {Object} reducers - The reducer functions for updating the state.
- *
- * @type {AuthSlice}
+ * This slice holds the state for the authentication
+ * status of the user and assigns the user details when logged in.
+ * @see {@link https://redux-toolkit.js.org/api/createSlice} for `createSlice` API.
  */
 import { createSlice } from "@reduxjs/toolkit";
 
-/**
- * Redux slice for managing the authentication state.
- *
- */
 const authSlice = createSlice({
   name: "auth",
   initialState: {
@@ -38,6 +13,19 @@ const authSlice = createSlice({
     user: null,
   },
   reducers: {
+    /**
+     * Sets the user as logged in and updates user information in the state.
+     *
+     * @param {Object} state - The current state of the auth slice.
+     * @param {Object} action - An action object containing the user information.
+     * @param {string} action.payload.userId - The user's unique identifier.
+     * @param {string} action.payload.email - The user's email address.
+     * @param {Array<string>} action.payload.groups - Groups the user belongs to.
+     * @param {string} action.payload.birthdate - The user's birth date.
+     * @param {string} action.payload.givenname - The user's given name.
+     * @param {string} action.payload.familyname - The user's family name.
+     */
+
     setUserLoggedIn: (state, action) => {
       state.isLoggedIn = true;
       state.user = action.payload.userId;
@@ -47,6 +35,11 @@ const authSlice = createSlice({
       state.givenname = action.payload.givenname;
       state.familyname = action.payload.familyname;
     },
+    /**
+     * Resets the authentication state, effectively logging the user out.
+     *
+     * @param {Object} state - The current state of the auth slice.
+     */
     setUserLoggedOut: (state) => {
       state.isLoggedIn = false;
       state.user = null;
--- frontend/src/reducers/store.js
+++ frontend/src/reducers/store.js
@@ -5,13 +5,40 @@ import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 import apigBaseUrlReducer from "./apigBaseUrl";
 
+/**
+ * Configuration for redux-persist to handle storage mechanisms.
+ *
+ * It determines how the state should be persisted in the local storage of the browser.
+ * The key 'root' is used as the storage key prefix for the persisted state.
+ *
+ * @type {Object}
+ */
+
 const persistConfig = {
   key: "root",
   storage,
 };
 
+/**
+ * A persisted reducer for the cart slice.
+ *
+ * Using redux-persist, the cart slice's state is persisted across browser sessions.
+ * This allows the cart's state to be rehydrated on app restarts, maintaining the cart contents.
+ *
+ * @type {Reducer}
+ */
 const persistedReducer = persistReducer(persistConfig, cartReducer);
 
+/**
+ * Configures and creates the Redux store.
+ *
+ * The store integrates different slices of the application state, applying middleware for side effects,
+ * and sets up persistence for the cart slice. The auth, cart, and API base URL slices are combined here.
+ *
+ * The middleware is customized to disable serializable checks, which is useful for certain types of middleware/actions.
+ *
+ * @returns {Store} A Redux store configured with reducers, middleware, and persistence capabilities.
+ */
 const store = configureStore({
   reducer: {
     auth: authReducer,
@@ -24,6 +51,14 @@ const store = configureStore({
     }),
 });
 
+/**
+ * The persistor for the Redux store.
+ *
+ * It is responsible for initiating the rehydration process of the persisted state
+ * from the local storage into the Redux store upon application startup.
+ *
+ * @type {Persistor}
+ */
 export const persistor = persistStore(store);
 
 export default store;
