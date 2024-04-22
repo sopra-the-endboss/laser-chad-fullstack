505a715e7d34951083d64ab42c1e40109240693f
Darnol <32634736+Darnol@users.noreply.github.com>
2024-04-22 11:54:48 +0200

    WIP : logout userId via localStorage

--- frontend/src/reducers/useAuth.js
+++ frontend/src/reducers/useAuth.js
@@ -25,6 +25,7 @@ const useAuth = () => {
   const apigBaseUrl = useSelector(state => state.apigBaseUrl);
   const cartItems = useSelector((state) => state.cart.cartItems);
   
+  // Login Cart
   const getCart = async (userId) => {
     // Send GET to get the cart
     // Return: Array with the fetched cart items
@@ -49,34 +50,6 @@ const useAuth = () => {
     } catch (error) {
       console.error(error);
       return([]);
-    }
-  }
-
-  const postCart = async (userId) => {
-    // Send POST to create a cart if not exists
-    try {
-      const settings = {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        }
-      };
-      const res = await fetch(apigBaseUrl + `/cart/${userId}`, settings);
-      const data = await res.json();
-
-      if (res.ok || res.status === 409) {
-        console.log(`DEBUG postCart : res.ok or duplicated, also fine`);
-      } else {
-        console.log(`ERROR Post cart for userId ${userId}, returned ${res.status}`);
-        console.log(`ERROR Post cart with data ${data}`);
-      }
-      console.log(`DEBUG getCart : res.statusCode ${res.status}`);
-      console.log(`DEBUG getCart : This is the data: ${JSON.stringify(data)}`);
-      return(data);
-
-    } catch (error) {
-      console.error(error);
     }
   }
 
@@ -113,14 +86,67 @@ const useAuth = () => {
     }
   }
 
+  // Logout Cart
+  const postCart = async (userId) => {
+    // Send POST to create a cart if not exists
+    try {
+      const settings = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+      };
+      const res = await fetch(apigBaseUrl + `/cart/${userId}`, settings);
+      const data = await res.json();
+
+      if (res.ok || res.status === 409) {
+        console.log(`DEBUG postCart : res.ok or duplicated, also fine`);
+      } else {
+        console.log(`ERROR Post cart for userId ${userId}, returned ${res.status}`);
+        console.log(`ERROR Post cart with data ${data}`);
+      }
+      console.log(`DEBUG getCart : res.statusCode ${res.status}`);
+      console.log(`DEBUG getCart : This is the data: ${JSON.stringify(data)}`);
+      return(data);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   const logoutCart = async () => {
     
-    let currentUserId = null;
-
-    // Get the user data
-    // console.log(`logoutCart: Get user data via localStorage`)
-    // console.log(`This is the persis:root in localStorage: ${JSON.stringify(localStorage.getItem("persist:root"))}`);
+    // Get the user data from localStorage
+    console.log(`logoutCart: Get user data via localStorage`)
+    let localStorageRaw = localStorage.getItem("persist:root");
+    console.log(`logoutCart: This is the raw persist:root ${localStorageRaw}`);
     
+    // // Remove extra escape characters
+    // const localStorageClean = localStorageRaw.replace(/\\"/g, '"');
+    // console.log(`logoutCart: This is the localStorageClean ${localStorageClean}`);
+    
+    // Parse the string into JSON object
+    const localStorageParsed = JSON.parse(localStorageRaw);
+    console.log(`logoutCart: This is the localStorageParsed: ${JSON.stringify(localStorageParsed)}`);
+    
+    const auth = localStorageParsed.auth;
+    console.log(`logoutCart: This is the auth: ${JSON.stringify(auth)}`);
+
+    // Remove extra escape characters
+    const authClean = auth.replace(/\\"/g, '"');
+    console.log(`logoutCart: This is the authClean ${authClean}`);
+    
+    const authParsed = JSON.parse(authClean);
+    console.log(`logoutCart: This is the authParsed ${JSON.stringify(authParsed)}`);
+    
+    const user = authParsed.user;
+    console.log(`logoutCart: This is the user ${JSON.stringify(user)}`);
+    
+    const userId = user.userId;
+    console.log(`logoutCart: This is the userId ${userId}`);
+    
+    postCart(userId);
     
     // const { attributes } = await CognitoAccount();
     // console.log(`logoutCart: This is the userId: ${attributes.sub}`);
@@ -183,7 +209,7 @@ const useAuth = () => {
         loginCart();
       } else if (event === "signedOut") {
         logoutCart();
-        // dispatch(setUserLoggedOut());
+        dispatch(setUserLoggedOut());
       } else if (event === "signedUp") {
       }
     });
